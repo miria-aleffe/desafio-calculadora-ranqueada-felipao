d5ab8fdedfd1ee145481c1166a1564d3278d4fc7
miria <miria_aleffe@hotmail.com>
2024-02-23 10:53:47 -0300

    minha finalizaçao

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-123456
+let vitoria = ("120")
+let derrota = ("10")
+
+function calcularNivel (vitorias, derrotas){
+    const saldoVitorias = vitoria - derrota;
+
+    let nivel;
+
+if (saldoVitorias < 10) {
+    nivel = "ferro"
+}
+else if (saldoVitorias >= 11 && saldoVitorias <=20) {
+    nivel = "bronze"
+}
+else if (saldoVitorias >= 21 && saldoVitorias <=50) {
+    nivel = "prata"
+}
+else if (saldoVitorias >= 51 && saldoVitorias <=80) {
+    nivel = "ouro"
+}
+else if (saldoVitorias >= 81 && saldoVitorias <=90) {
+    nivel = "diamante"
+}
+else if (saldoVitorias >= 91 && saldoVitorias <=100) {
+    nivel = "lendario"
+}
+else if (saldoVitorias >= 101) {
+    nivel = "Imortal"
+}
+
+return (" O herói tem um saldo de " + saldoVitorias + " e está no nível " + nivel)
+}
+
+let resultado = calcularNivel(vitoria, derrota)
+console.log(resultado)
